33069fe01690293d3d7774339b02628a6f3157d1
justaguy6 <outerjose@gmail.com>
2025-12-31 17:08:37 -0300

    pouca coisa

--- source/important/CoolUtil.hx
+++ source/important/CoolUtil.hx
@@ -7,7 +7,7 @@ import lime.utils.Assets as LimeAssets;
 import lime.utils.AssetLibrary;
 import lime.utils.AssetManifest;
 import flixel.sound.FlxSound;
-#if sys
+#if desktop
 import sys.io.File;
 import sys.FileSystem;
 #else
@@ -57,7 +57,7 @@ class CoolUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if sys
+		#if desktop
 		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
 		#else
 		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
